0d1eafaf14b1d7b99d5f0e9ea8eeaf1de7f155db
Kalle Tolonen <kalle.tolonen@gmail.com>
2022-01-31 20:35:53 +0200

    Exercise 2.3 done

--- Exercise 2/Exercise 2_3.py
+++ Exercise 2/Exercise 2_3.py
@@ -3,7 +3,7 @@
 # Finally, calculate the means, medians, standard deviations, and variances of the lengths and weights.
 #Extra: Draw a histogram pattern of the lengths
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 data = np.genfromtxt("weight-height.csv",delimiter=",", dtype=str, skip_header=1)
 
@@ -13,24 +13,41 @@ print(dataB[0][1], "inches")
 print(dataB[0][2], "pounds")
 
 for i in dataB:
-    i[1] = np.double(i[1])*2.54 #convert values to cm
-    i[2] = np.double(i[2])*0.453592 #convert values to kg
+    i[1] = float(i[1])*2.54 #convert values to cm
+    i[2] = float(i[2])*0.453592 #convert values to kg
 
-print(dataB[0][1], "cm")
+print(dataB[0][1], "cm", dataB[0][1].dtype)
 print(dataB[0][2], "kg")
 
 #mean
 weightList = []
-print(len(dataB))
-for i in dataB:
-    weightList.append(i[1])
+heightList = []
 
-print(len(weightList))
+for i in dataB:
+    heightList.append(float(i[1]))
+    weightList.append(float(i[2]))
+    
+print(heightList[0])
+print("The mean of heights is ", np.mean(heightList), " cm.")
+print("The mean of weights is ", np.mean(weightList), " kg.")
 
 #median
+print("The median of heights is ", np.median(heightList), " cm.")
+print("The median of weights is ", np.median(weightList), " kg.")
 
 #standard deviation
+print("The standard deviation of height is ", np.std(heightList))
+print("The standard deviation of weight is ", np.std(weightList))
 
 #variations
+print("The variance of height is ", np.var(heightList))
+print("The variance of weight is ", np.var(weightList))
 
 #histogram
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Height & weight histograms')
+ax1.hist(heightList, bins=50)
+ax1.set(xlabel='Height', ylabel='frequency')
+ax2.hist(weightList, bins=50)
+ax2.set(xlabel='Weight')
+plt.show()
